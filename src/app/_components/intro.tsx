1dc13041276c80bf3a6cfa300776c167c492cea2
GeorgeGarcia11UTRGV <george.garcia11@utrgv.edu>
2025-04-22 10:29:43 -0500

    Update intro.tsx

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -7,14 +7,7 @@ export function Intro() {
         Game Designer
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Created this website by using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-blue-600 duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and {CMS_NAME}.
+        George Garcia Spring 2025 
       </h4>
     </section>
   );
